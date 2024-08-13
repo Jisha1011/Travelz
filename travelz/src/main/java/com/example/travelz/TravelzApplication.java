6097f3e0e7f8edfaf3665541986732cd2c7fe729
Jisha1011 <azijisha@gmail.com>
2024-08-13 16:14:24 +0400

    Initial setup

---
+++ travelz/src/main/java/com/example/travelz/TravelzApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelzApplication.class, args);
+	}
+
+}
